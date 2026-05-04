2ecbcee0bd32829b5e7c3c606e410f3503e844f6
Tona Bissessar <ana_lisa8@hotmail.com>
2026-05-04 14:08:56 +0100

    Register the Bookings Model and the Table model in admin.py and import it.

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from bookings.models import Booking
+from bookings.models import Table
 
 # Register your models here.
+admin.site.register(Booking)
+admin.site.register(Table)
--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+STATUS = ((0, 'Pending'), (1, 'Confirmed'), (2, 'Cancelled'), (3, 'Completed'))
 
 # Create your models here.
+class Booking(models.Model):
+    booking_id = models.AutoField(primary_key=True)
+    User_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    table_id = models.ForeignKey('Table', on_delete=models.CASCADE)
+    party_number = models.PositiveIntegerField(blank=False, validators=[MinValueValidator(1), MaxValueValidator(20)])
+    booking_date = models.DateField(blank=False)
+    booking_time = models.TimeField(blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.PositiveIntegerField(choices=STATUS, default=0)
+    
+
+    def __str__(self):
+        return f"Booking for {self.User_id.first_name} {self.User_id.last_name} on {self.booking_date} at {self.booking_time} for {self.party_number} people."
+    
+
+
+class Table(models.Model):
+    table_id = models.AutoField(primary_key=True)
+    table_number = models.PositiveIntegerField(validators=[MinValueValidator(20), MaxValueValidator(30)], blank=False)
+    seating_capacity = models.PositiveIntegerField(blank=False)
+
+    def __str__(self):
+        return f"Table {self.table_number} (Seats {self.seating_capacity})"
