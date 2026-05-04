1960609ef4bd893f1fd8dd8a4536ee79a7180bd6
Tona Bissessar <ana_lisa8@hotmail.com>
2026-05-04 21:25:26 +0100

    map properly the views and URL with settings

--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,62 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegistrationForm
+from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+from django.template import Context
 from django.http import HttpResponse
 
 # Create your views here.
-def my_bookings(request):
-    return HttpResponse("This is the bookings page.")
+#def my_bookings(request):
+#    return HttpResponse("This is the bookings page.")
+
+# for index page
+def index(request):
+    return render(request, 'bookings/index.html', {'title': 'index'})
+
+# for registration page
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            phone_no = form.cleaned_data.get('phone_no')
+            htmly = get_template('bookings/Email.html')
+            d = { 'username': username }
+            subject, from_email, to = 'welcome', 'your_email@gmail.com', email
+            html_content = htmly.render(d)
+            msg = EmailMultiAlternatives(subject, html_content, from_email, [to])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+            messages.success(request, f'Account created for {username}! You can now Log in.')
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'bookings/register.html', {'form': form, 'title': 'register here'}) 
+
+# for login page
+def Login(request):
+    if request.method == 'POST':
+ 
+        # AuthenticationForm_can_also_be_used__
+ 
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            form = login(request, user)
+            messages.success(request, f' Hello, {username} !!')
+            return redirect('index')
+        else:
+            messages.info(request, f'account done not exit plz sign in')
+    form = AuthenticationForm()
+    return render(request, 'bookings/login.html', {'form':form, 'title':'log in'}) 
+
