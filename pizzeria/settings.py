ccd80d97c17020e9b958f6c74c1fdd5be41608eb
Tona Bissessar <ana_lisa8@hotmail.com>
2026-05-08 11:37:24 +0100

    remove email back end for prod

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -139,4 +139,3 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
