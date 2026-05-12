5a6e4257a23e41f15252c9a00a45261dbb07f35e
Tona Bissessar <ana_lisa8@hotmail.com>
2026-05-12 10:16:12 +0100

    Adjust URL paths to include staff_portal, staff_dashboard and staff_register HTML pages.

--- bookings/urls.py
+++ bookings/urls.py
@@ -13,5 +13,9 @@ urlpatterns = [
          path('my_bookings/', views.my_bookings, name='my_bookings'),
          path('update_booking/<int:booking_id>/', views.update_booking, name='edit_booking'),
          path('cancel/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
+         path('login-check/', views.login_redirect, name='login_redirect'),
+         path('staff-portal/', views.staff_portal_view, name='staff_portal'),
+         path('staff-register/', views.staff_register_customer, name='staff_register_customer'),
+         path('staff-dashboard/', views.staff_dashboard_view, name='staff_dashboard'),
 
 ]
